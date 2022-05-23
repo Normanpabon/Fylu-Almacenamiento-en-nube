d3d1bea882adbb467f16d7f2e6ccc5eff80e1f46
Norman P <45250874+Normanpabon@users.noreply.github.com>
2022-05-23 16:33:27 -0500

    Version 0.1
    Partes funcionales:
    
    - creacion de usuario
    - logueo de usuario
    - modificaciones al usuario (clave y correo)
    - boton de darse de baja
    
    - subida de archivos a la bd
    - registro de archivos subidos a la bd
    - metodos para llevar control de almacenamiento de usuario y cuotas

---
+++ CloudDrive/grails-app/controllers/clouddrive/ArchivoController.groovy
@@ -0,0 +1,197 @@
+package clouddrive
+
+import grails.validation.ValidationException
+import static org.springframework.http.HttpStatus.*
+import java.io.File
+
+class ArchivoController {
+
+    static String pathToData = new File(".").getCanonicalPath()
+
+    ArchivoService archivoService
+    UsuarioService usuarioService
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+
+    // se puede usar tambien para verificar si existe el directorio
+    boolean existeArchivo(String path){
+
+        def file = new File(path)
+
+        if(file.exists()){
+            return true
+        }else{
+            return false
+        }
+
+    }
+
+     def listarArchivos(){
+
+        def fileList = Archivo.findAllByUid_usr(Usuario.get(session.user))
+
+        if(fileList != null){
+            params.myFileList = fileList
+        }
+        
+        
+       
+    }
+
+    def calcularEspacioUsado(){
+
+        def fileList = Archivo.findAllByUid_usr(Usuario.get(session.user))
+        def acum = 0
+
+        for(int i =0; i < fileList.size(); i++){
+            acum += fileList[i].size
+        }
+        
+        return acum
+        
+    }
+
+
+    def calcularEspacioDisponible(){
+
+        def espacioUsado = calcularEspacioUsado()
+        
+        return espacioUsado - (Usuario.get(session.user)).cuota
+
+        //render ("espacio usado= " calcularEspacioUsado().toString() + "MB\t" + "espacio disponible = " + (espacioUsado - (Usuario.get(session.user)).cuota).toString() + "MB")
+    }
+
+    def crearDirectorioUsuario(String path){
+
+        def file = new File(path)
+        file.mkdir()
+
+    }
+
+    def descargarArchivo(){
+        // espera params.fileToDownloadId
+        def fileId = params.fileToDownloadId
+        def tmpFile = new File(pathToData+(archivoService.get(fileId)).file_path)
+        if(tmpFile.exists()){
+            //falta probar, en espera de la vista
+            response.setContentType("application/octet-stream")
+            response.setHeader("Content-disposition", "attachment;filename=\"${tmpFile.name}\"")
+            response.outputStream << tmpFile.bytes
+                
+        }else{
+            render "Error al descargar el archivo"
+        }
+    }
+        
+    // anadir logica de sobrecarga para descargar multiples archivos
+    /*
+    def descargarArchivo(){
+
+    }
+    */
+
+    def eliminarArchivo(){
+        def tmpArchivo = archivoService.get(session.idDelArchivo)
+
+        try{
+            tmpArchivo.delete(flush:true)
+            // todo probar borrar archivo del disco tambien
+            new File(pathToData+tmpArchivo.file_path).delete()
+
+        }catch(org.springframework.dao.DataIntegrityViolationException e){
+
+        }
+    }
+
+
+
+    def subirArchivo(){
+        def archivo = request.getFile('myFile')
+
+        String tmpUser = (Usuario.get(session.user)).nombre_usuario + "\\"
+        // todo: corregir por nombre original del archivo
+        String tmpFileName = archivo.getOriginalFilename()
+        String tmpPath = pathToData+"\\data\\"+tmpUser
+
+        // condicional para creacion de directorio
+        if(!existeArchivo(tmpPath)){
+            crearDirectorioUsuario(tmpPath)   
+        }
+
+        tmpPath += tmpFileName
+
+        if(archivo.empty){
+            render "Error el archivo esta vacio \nDEBUG params : "+ params + " \n DEBUG request: " + request + " \n DEBUG session: " + session
+
+            //todo : aca manejar el error mostrando una ventana emergente o algo
+
+        }else{
+            
+            // llamar a accion de crear archivo (dejar registro en la bd)
+
+            //verificar si el archivo ya existe, si existe sobrescribir y editar registro en la bd
+            if(existeArchivo(tmpPath)){
+                print("terminar")
+                modArchivo(tmpFileName, ((archivo.getSize()*0.000001).round(3)))
+            }else{
+                crearArchivo("\\data\\"+tmpUser+tmpFileName, tmpFileName, ((archivo.getSize()*0.000001).round(3)))
+            }
+            
+            
+            // transfiere el archivo a la ubicacion dada
+            archivo.transferTo(new File(tmpPath))
+
+            //render " se ha creado y transferido correctamente \nDEBUG params : "+ params + " \n DEBUG request: " + request + " \n DEBUG session: " + session
+            render "has encontrado un bug we"
+            // todo : una vez finalizada la subida, recargar la vista
+            
+        }
+
+        
+    }
+
+    def modArchivo(nombre, fSize){
+        def tmpArchivo = Archivo.findByNombre(nombre)
+
+        if(tmpArchivo != null){
+            // actualiza el tamaño del archivo
+            tmpArchivo.size = fSize
+
+            try {
+                archivoService.save(tmpArchivo)
+                flash.message = "Archivo modificado correctamente"
+                redirect(controller: "usuario", action: "dirigirHome")
+            } catch (ValidationException e) {
+                
+            }
+            
+        }else{
+            // aca podria entrar si el archivo existe en la carpeta pero no en la bd
+            redirect(controller: "usuario", action: "dirigirHome")
+        }
+    }
+
+    def crearArchivo(tPath, nombre, fSize){
+        // todo calcular tamaño de archivo o ver como pasarlo por parametro
+
+        def tmpArchivo = new Archivo(uid_usr:Usuario.get(session.user), size:fSize, file_path:tPath, nombre:nombre)
+        // todo: verificar que el archivo no exista
+        if(true){
+            try{
+                archivoService.save(tmpArchivo)
+
+                flash.message = 'Archivo creado correctamente '
+                redirect(controller: "usuario", action: "dirigirHome")
+            }catch(ValidationException e){
+                render "Error al subir el archivo \nDEBUG params : "+ params + " \n DEBUG request: " + request + " \n DEBUG session: " + session + " \n DEBUG path: " + tPath + "\n\n TmpArchivoData: " +tmpArchivo
+            }
+        
+        }else{
+            // todo: notificar que el archivo ya existe
+        }
+
+    }
+
+   
+}
---
+++ CloudDrive/grails-app/views/usuario/home.gsp
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Bienvenido!</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/x-ico" href="/assets/favicon.ico">
+        <%-- ======= BOX ICONS ======= --%>
+        <asset:stylesheet href="boxicons.min.css"/>
+        <%-- ======= CSS ======= --%>
+        <asset:stylesheet href="home.css"/>
+    </head>
+    <body>
+        <!--=============== NAV ===============-->
+        <div class="nav" id="nav">
+            <nav class="nav__content">
+                <div class="nav__toggle" id="nav-toggle">
+                    <i class='bx bx-chevron-right' ></i>
+                </div>
+    
+                <a href="#" class="nav__logo">
+                    <i class='bx bx-cloud-drizzle'></i>
+                    <span class="nav__logo-name">Fylu</span>
+                </a>
+    
+                <div class="nav__list">
+                    
+                    <g:link class="nav__link" action="dirigirHome"><g:message args="[entityName]" />
+                        <i class='bx bx-grid-alt'></i>
+                        <span class="nav__name">Inicio</span>
+                    </g:link>
+    
+                    <g:link class="nav__link" action="dirigirHome"><g:message args="[entityName]" />
+                        <i class='bx bx-file'></i>
+                        <span class="nav__name">Archivos</span>
+                    </g:link>
+    
+    
+                    <a href="#" class="nav__link">
+                        <i class='bx bx-share-alt' ></i>
+                        <span class="nav__name">Compartidos</span>
+                    </a>
+    
+                    <g:link class="nav__link" action="cerrarSesion"><g:message args="[entityName]" />
+                        <i class='bx bx-log-out'></i>
+                        <span class="nav__name">Salir</span>
+                    </g:link>
+
+                    <g:link class="nav__link" action="dirigirPerfil"><g:message args="[entityName]" />
+                        <i class='bx bx-cog' ></i>
+                        <span class="nav__name">Ajustes</span>
+                    </g:link>
+
+                </div>
+            </nav>
+        </div>
+        <!--=============== MAIN ===============-->
+        <main class="container section">
+            <h1>Mis archivos</h1>
+            <h2>Aca deberia ir el menu con los archivos y eso</h2>
+
+            <!--- ciclo para mostrar los archivos -->
+            <g:each in="${params.myFileList}" var="file">
+             <li>${file.nombre}   ${file.size}MB</li>
+            </g:each>
+
+            <!-- subida de archivos  -->
+            <h3> Prueba subida archivos </h3>
+            <br />
+            <g:uploadForm controller="Archivo" action="subirArchivo">
+                <input type="file" name="myFile" />
+                <input type="submit" name="Subir archivo" />
+            </g:uploadForm>           
+        </main>
+        <!--=============== HOME JS ===============-->
+        <asset:javascript src="home.js"/>
+    </body>
+</html>
